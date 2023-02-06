87146e29dc6472d360c9e455a53037ec50d96448
Dhruvkumar Patel <122956998+PatidarDhruv07@users.noreply.github.com>
2023-02-06 15:59:36 -0500

    Create Lab 2.py

---
+++ Lab 2.py
@@ -0,0 +1,31 @@
+def main():
+    return
+
+#TODO: Step 2 - Create a complex data structure
+about_me = {}
+
+#TODO: step 3 - Add another movie to the data structure
+
+#TODO: step 4 - fuction that print name and ID
+def print_student_name_and_id(about_me):
+    return
+
+#TODO: step 5 - fuction that add pizza topings to data structure
+def add_pizza_topping(about_me, topping):
+    return
+
+#TODO: step 6 - function that prints bullet list of pizza toppings
+def print_pizza_topping(about_me):
+    return
+
+#TODO: step 7 - function that prints comma-seperated list of movie genres
+def print_movie_genres(about_me):
+    return
+
+#TODO: step 8 - function that prints comma-seperated list of movie titles
+def print_movie_titles(movie_list):
+    return
+
+
+if __name__ == '__main__':
+    main()
